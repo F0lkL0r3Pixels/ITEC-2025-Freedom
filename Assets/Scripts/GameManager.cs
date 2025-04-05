eef0cfacd679a2532244b47cdd022023e5e1fa81
Sarugami Keukegen <Eduard Scobioala>
2025-04-05 16:30:51 +0300

    add stamina bar

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -34,6 +34,6 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        UIManager = GetComponent<UIManager>();
+        UIManager = GetComponentInChildren<UIManager>();
     }
 }
