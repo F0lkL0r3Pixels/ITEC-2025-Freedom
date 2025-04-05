b74347c0d9c146d84a3c5415405818d71877f23e
Tudypie <33548577+Tudypie@users.noreply.github.com>
2025-04-05 23:04:08 +0300

    Upgrades & more platforming

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -5,6 +5,9 @@ public class PlayerManager : MonoBehaviour
     public PlayerMovement Movement;
     public Grappling Grappling;
 
+    [TextArea(2, 5)]
+    public string[] upgradeMessage;
+
     public static PlayerManager Instance;
 
     private void Awake()
@@ -18,4 +21,23 @@ public class PlayerManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    public void GetUpgrade(int index)
+    {
+       GameManager.UIManager.infoBarController.ShowMessage(upgradeMessage[index]);
+
+        switch (index)
+        {
+            case 0:
+                Movement.CanChargeJump = true;
+                break;
+            case 1:
+                Movement.CanDash = true;
+                break;
+            case 2:
+                Grappling.enabled = true;
+                break;
+        }
+    }
+
 }
